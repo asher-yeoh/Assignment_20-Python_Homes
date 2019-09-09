f3e70f76aebbe2df6ed5514b955e79416f30d66a
ASHER <asher.yeoh@dell.com>
2019-09-09 22:27:33 +0800

    completed documentation

--- schemas.py
+++ schemas.py
@@ -3,13 +3,6 @@ from models import Home
 
 from marshmallow import Schema, fields, pprint
 
-# class HomeSchema(ModelSchema):
-#     class Meta:
-#         model = Home
-
 class HomeSchema(ModelSchema):
-    uppername = fields.Function(lambda obj: obj.name.upper())
-
     class Meta:
-        fields = ("id", "sell", "list", "living", "rooms", "beds", "baths", "age", "acres", "taxes")
-        ordered = True
+        model = Home
